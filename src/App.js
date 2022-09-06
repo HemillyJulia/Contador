772a49746890219f78e6d85ca22957000b4a1395
HemillyJulia <hemillyjulia@hotmail.com>
2022-09-05 22:00:37 -0400

    Criando um contador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {useState} from "react"
+import styled from 'styled-components'
+import Meubotao from "./Componentes/Meubotao"
+
+const Fundo = styled.div `
+background-color:blueviolet
+`
 
 function App() {
+
+//Crio duas variáveis , a primeira é o valor que eu quero guardar e a segunda
+// é a função que atualiza o valor guardado
+const [numero, setNumero] = useState(0)
+
+//primeiramente criei a função soma que recebe a função set dentro dela , na qual
+// o numero que é o meu valor guardado, ao ser clicado, será somado +1
+function somar () {
+ setNumero (numero + 1)
+}
+
+//depois criei a função subtrair que recebe a função set dentro dela , na qual
+// o numero que é o meu valor guardado, ao ser clicado, será subtraído -1
+function diminuir () {
+ setNumero (numero - 1)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fundo >
+   <h1>Contador</h1>
+{/* // abaixo eu coloquei a variável número, que é o meu o meu valor inicial. */}
+   <p>{numero}</p> 
+{/* //abaixo eu criei dois botões que irão fazer a lógica descrita acima */}
+   <Meubotao onClick={somar}>Somar</Meubotao>
+   <button onClick={diminuir}>Subtrair</button>
+
+    </Fundo>
   );
 }
 
